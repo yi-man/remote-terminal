0f4f0e7bbc26623c9b59e1b50c20ab858b224a12
许鑫 <xuxin@nl-ds.cn>
2026-03-17 14:57:30 +0800

    feat: add AppRoutes component

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ConnectionListPage } from '../pages/ConnectionListPage';
+import { ConnectionCreatePage } from '../pages/ConnectionCreatePage';
+import { ConnectionEditPage } from '../pages/ConnectionEditPage';
+import { TerminalPage } from '../pages/TerminalPage';
+import { PageLayout } from '../layouts/PageLayout';
+import { useUserId } from '../hooks/useUserId';
+
+export function AppRoutes() {
+  const userId = useUserId();
+
+  if (!userId) {
+    return (
+      <div className="flex items-center justify-center h-full bg-gray-900 text-white">
+        <div>加载中...</div>
+      </div>
+    );
+  }
+
+  return (
+    <BrowserRouter>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<ConnectionListPage />} />
+          <Route path="/create" element={<ConnectionCreatePage />} />
+          <Route path="/connections/:id/edit" element={<ConnectionEditPage />} />
+          <Route path="/connections/:id/terminal" element={<TerminalPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </PageLayout>
+    </BrowserRouter>
+  );
+}
